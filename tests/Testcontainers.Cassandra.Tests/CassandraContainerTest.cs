29f1c403eb111c7b5028acf01a2ffbdba4ec32a7
Gavin de Kock <gavindekock@users.noreply.github.com>
2024-10-12 17:45:09 +0100

    added test to confirm running cql using driver

--- tests/Testcontainers.Cassandra.Tests/CassandraContainerTest.cs
+++ tests/Testcontainers.Cassandra.Tests/CassandraContainerTest.cs
@@ -32,17 +32,41 @@
 
     [Fact]
     [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-    public async Task ExecScriptReturnsSuccessful()
+    public async Task DriverExecutesCqlStatementAndReturnResult()
     {
       // Given
-      const string scriptContent = "SELECT * FROM system.local;";
+      const string selectFromSystemLocalStatement = "SELECT * FROM system.local WHERE key = ?;";
+      var cluster = Cluster.Builder()
+        .AddContactPoint(_cassandraContainer.GetEndPoint())
+        .Build();
 
       // When
-      var execResult = await _cassandraContainer.ExecScriptAsync(scriptContent)
+      var session = await cluster.ConnectAsync();
+      var preparedStatement = await session.PrepareAsync(selectFromSystemLocalStatement);
+      var boundStatement = preparedStatement.Bind("local");
+      var result = await session.ExecuteAsync(boundStatement);
+
+      // Then
+      Assert.True(result.IsFullyFetched);
+      var resultRows = result.GetRows().ToList();
+      Assert.Single(resultRows);
+      Assert.Equal("COMPLETED", resultRows.First()["bootstrapped"]);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ExecScriptAsyncReturnsSuccess()
+    {
+      // Given
+      const string selectFromSystemLocalStatement = "SELECT * FROM system.local;";
+
+      // When
+      var execResult = await _cassandraContainer.ExecScriptAsync(selectFromSystemLocalStatement)
         .ConfigureAwait(true);
 
       // Then
       Assert.True(0L.Equals(execResult.ExitCode), execResult.Stderr);
+      Assert.NotEmpty(execResult.Stdout);
       Assert.Empty(execResult.Stderr);
     }
   }
